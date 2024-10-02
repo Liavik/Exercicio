acd9def887c025edd1c41771a32d1fa2a01e863f
Julia Machado <julia-machado@outlook.com>
2024-10-02 11:45:11 -0300

    reset

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
-$(document).ready(function () {
-    $('#carousel-imagens').slick({
-        autoplay: true,
-        arrows: false
+$(document).ready(function() {
+    $('#carousel-imagem').slick({
+        autoplay:true
     })
 
-    $('#telefone').mask('(00) 00000-0000', {
-        placeholder: '(DDD) 12345-6789'
+    $('.menu-hamburguer').click(function() {
+        $('nav').slideToggle();
     })
 
-    $('#cpf').mask('000.000.000-00', {
-        placeholder: '123.456.789-00'
-    })
-
-    $('#cep').mask('00000-000', {
-        placeholder: '012345-678'
-    })
+    $('#telefone').mask('(00) 00000-0000')
 
     $('form').validate({
         rules: {
@@ -28,26 +21,36 @@ $(document).ready(function () {
             telefone: {
                 required: true
             },
-            endereco: {
+            mensagem: {
                 required: true
             },
-            cep: {
-                required: true
-            },
-            cpf: {
-                required: true
+            veiculoDeInteresse: {
+                required: false
             },
         },
-        submitHandler: function (form) {
-            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-            form.reset();
+        messages: {
+            nome: 'Por favor, insira o seu nome'
         },
-        invalidHandler: function (form, validator) {
-            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
         }
     })
+
+    $('.lista-veiculos button').click(function() {
+        const destino = $('#contato');
+
+        const nomeVeiculo = ($(this).parent().find('h3').text());
+
+        $('#veiculo-interesse').val(nomeVeiculo)
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
 })
-
-
-
-
